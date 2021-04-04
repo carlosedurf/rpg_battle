249b1ac0465c39f251d70a003bf30c545f589f73
Carlos Eduardo <carloseduardo.rocha.f@gmail.com>
2021-04-04 20:06:49 -0300

    feat: Adicione as rotas vinculadas com  controlers

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{
+    BattleController,
+    ClassificationController,
+    UserController,
+    HeroController,
+    MonsterController
+};
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +20,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('/users')->name('users.')->group(function (){
+
+    Route::get('{user}', [UserController::class, 'show'])->name('show');
+    Route::get('{user}/battles', [UserController::class, 'battles'])->name('battles');
+    Route::post('create', [UserController::class, 'store'])->name('create');
+
 });
+
+
+Route::prefix('/battles')->name('battles.')->group(function (){
+
+    Route::post('{battle}/round', [BattleController::class, 'round'])->name('round');
+    Route::get('{battle}', [BattleController::class, 'show'])->name('show');
+    Route::get('{battle}/history', [BattleController::class, 'history'])->name('history');
+
+    Route::post('start', [BattleController::class, 'start'])->name('start');
+
+});
+
+Route::get('/classifications', [ClassificationController::class, 'index'])->name('classifications.index');
+
+Route::get('/heroes', [HeroController::class, 'index'])->name('heroes.index');
+Route::get('/monster', [MonsterController::class, 'index'])->name('monsters.index');
